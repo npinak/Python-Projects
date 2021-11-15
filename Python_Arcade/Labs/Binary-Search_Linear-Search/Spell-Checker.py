5dc7d5844f2dfd4ef853064f6e44871db94fb2f2
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2021-11-15 13:51:26 +0530

    New Search Lab

--- Python_Arcade/Labs/Binary-Search_Linear-Search/Spell-Checker.py
+++ Python_Arcade/Labs/Binary-Search_Linear-Search/Spell-Checker.py
@@ -23,26 +23,57 @@ dictionary_file.close()
 
 
 ##### Doing the linear search #####
-'''
+
 print("--- Linear Search ---")
 
 
 story_file = open("AliceInWonderLand200.txt")
 
 word_list = []
+line_count = 0
 
 for line in story_file:
     word_list = split_line(line)
+    line_count += 1
     for word in word_list:
         current_list_position = 0
         while current_list_position < len(dictionary_list) and dictionary_list[current_list_position] != word.upper():
             current_list_position += 1
         if current_list_position == len(dictionary_list):
-            print(word)
+            print(f"Line {line_count} possible misspelled word: {word}")
         else:
             continue
-'''
+
+
+##### Doing the Binary Search #####
+
 print("--- Binary Search ---")
+
+
+story_file = open("AliceInWonderLand200.txt")
+
+word_list = []
+line_count = 0
+
+for line in story_file:
+    word_list = split_line(line)
+    line_count += 1
+    for word in word_list:
+        lower_bound = 0
+        upper_bound = len(dictionary_list) - 1
+        found = False
+        while lower_bound < upper_bound and found == False:
+            middle_pos = (lower_bound + upper_bound) // 2
+            if dictionary_list[middle_pos] < word.upper():
+                lower_bound = middle_pos + 1
+            elif dictionary_list[middle_pos] > word.upper():
+                upper_bound = middle_pos
+            else:
+                found = True
+        if found == False:
+            print(f"Line {line_count} possible misspelled word: {word}")
+        else:
+            continue
 
 
 
