e96700bb746726c3d873373041c6c433edcb9105
Pinak Nayak <77262741+npinak@users.noreply.github.com>
2022-05-01 15:23:36 -0700

    Enemies Firing, but not independently of each other

--- Python_Arcade/Labs/Iridion/save1.py
+++ Python_Arcade/Labs/Iridion/save1.py
@@ -1,75 +1,60 @@
 import random
 import arcade
 import math
-import os
 
+SPRITE_SCALING_PLAYER = 0.5
+SPRITE_SCALING_COIN = 0.2
+SPRITE_SCALING_LASER = 0.8
 
-SPRITE_SCALING = 0.5
-SPRITE_SCALING_LASER = 0.3
-SPRITE_SCALING_ENEMY = 0.5
+COIN_COUNT = 50
+ENEMY_COUNT = 10
 
-BULLET_SPEED = 10
-ANGLE_SPEED = 5
 MOVEMENT_SPEED = 5
-REG_ENEMY_SPEED = 2
+TURN_SPEED  = 5
+BULLET_SPEED = 5
+REG_ENEMY_SPEED = 3
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Iridion Remake"
 
-ENEMY_COUNT = 10
+REG_ENEMY_RESPAWN = 5
 
 
-class Player(arcade.Sprite):
+# Extras to add
 
-    def update(self):
-        """ Move the player """
-        # Move player.
-        # Remove these lines if physics engine is moving player.
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-
-        # Check for out-of-bounds
-        if self.left < 0:
-            self.left = 0
-        elif self.right > SCREEN_WIDTH - 1:
-            self.right = SCREEN_WIDTH - 1
-
-        if self.bottom < 0:
-            self.bottom = 0
-        elif self.top > SCREEN_HEIGHT - 1:
-            self.top = SCREEN_HEIGHT - 1
+# - Remove Ship turning capability 
+#̶ - A̶d̶d̶ b̶e̶t̶t̶e̶r̶ m̶o̶v̶e̶ b̶y̶ k̶e̶y̶b̶o̶a̶r̶d̶ (̶h̶t̶t̶p̶s̶:̶/̶/̶a̶p̶i̶.̶a̶r̶c̶a̶d̶e̶.̶a̶c̶a̶d̶e̶m̶y̶/̶e̶n̶/̶l̶a̶t̶e̶s̶t̶/̶e̶x̶a̶m̶p̶l̶e̶s̶/̶s̶p̶r̶i̶t̶e̶_̶m̶o̶v̶e̶_̶k̶e̶y̶b̶o̶a̶r̶d̶_̶b̶e̶t̶t̶e̶r̶.̶h̶t̶m̶l̶?̶h̶i̶g̶h̶l̶i̶g̶h̶t̶=̶k̶e̶y̶b̶o̶a̶r̶d̶)̶
+# - M̶a̶k̶e̶ e̶n̶e̶m̶i̶e̶s̶ s̶p̶a̶w̶n̶ a̶n̶d̶ f̶o̶l̶l̶o̶w̶ t̶h̶e̶ p̶l̶a̶y̶e̶r̶
+    # - M̶a̶k̶e̶ e̶n̶e̶m̶i̶e̶s̶ s̶p̶a̶w̶n̶ a̶b̶o̶v̶e̶ t̶h̶e̶ s̶c̶r̶e̶e̶n̶ b̶o̶r̶d̶e̶r̶
+    # - A̶d̶d̶ r̶e̶s̶p̶a̶w̶n̶i̶n̶g̶ c̶a̶p̶a̶b̶i̶l̶i̶t̶y̶
+    # - Make Enemies Shoot 
+    # - Make enemies stay above a certain line. 
+    # - Make enemies crossover at screen border
+    # - If enemy touches player it gets destroyed and the player loses health
+    # - Add Pro Enemy
+    # - Make enemy angle up to the player before shooting (backburner)
+        # - print(math.degrees(angle))
+        # - self.angle = angle #### Test Code #### - Gotta change from radians to deg
+# - Add a moving background 
+# - Make power-up 
+# - Make player ship crossover at screenborder 
+# - Give player healthbar 
 
 class Reg_Enemy(arcade.Sprite):
 
-    def __init__(self, name_folder, name_file):
-        super().__init__()
+    def __init__(self, filename, scale):
+        """ Set up the space ship. """
 
-        # Creating bullet list for reg_aim method
-        self.reg_enemy_bullet_list = arcade.SpriteList()
+        # Call the parent Sprite constructor
+        super().__init__(filename, scale)
 
-    def follow_sprite(self, player_sprite):
-        """
-        This function will move the current sprite towards whatever
-        other sprite is specified as a parameter.
+        self.bullet_list = arcade.SpriteList()
 
-        We use the 'min' function here to get the sprite to line up with
-        the target sprite, and not jump around if the sprite is not off
-        an exact multiple of SPRITE_SPEED.
-        """
+    def setup(self):
+        
+        self.bullet_list = arcade.SpriteList()
 
-
-        if self.center_y > SCREEN_HEIGHT:
-            self.change_y = -2
-
-        if self.center_y < 0:
-            self.change_y = +2
-
-        if self.center_x > SCREEN_WIDTH:
-            self.change_x = -2
-
-        if self.center_x < 0:
-            self.change_x = 2
+    def follow_player(self, player_sprite):
 
         self.center_x += self.change_x
         self.center_y += self.change_y
@@ -96,52 +81,81 @@ class Reg_Enemy(arcade.Sprite):
             self.change_x = math.cos(angle) * REG_ENEMY_SPEED
             self.change_y = math.sin(angle) * REG_ENEMY_SPEED
 
-    '''def reg_aim(self, player_sprite):
+    def shoot(self, player_sprite):
 
-        #for enemy in enemy_list:  # we won't be needing this line later. Just for context
+        print(len(self.bullet_list))
+        self.bullet_list.draw()
+        
+        # Random 1 in 100 chance that we'll change from our old direction and
+        # then re-aim toward the player
+        if random.randrange(100) == 0:
+            start_x = self.center_x
+            start_y = self.center_y
 
-        start_x = self.center_x
-        start_y = self.center_y
+            # Get the destination location for the bullet
+            dest_x = player_sprite.center_x
+            dest_y = player_sprite.center_y
 
-        dest_x = player_sprite.center_x
-        dest_y = player_sprite.center_y
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
 
-        x_diff = dest_x - start_x
-        y_diff = dest_y - start_y
-
-        angle = math.atan2(y_diff, x_diff)
-
-        reg_enemy_bullet = arcade.Sprite("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/"
-                                         "Iridion/Game_Assets/Active_use/Lasers/spaceMissiles_038.png",
-                                         SPRITE_SCALING_LASER)
-        reg_enemy_bullet.angle = angle
-        reg_enemy_bullet.change_y = -math.cos(math.radians(player_sprite.angle)) * BULLET_SPEED
-        reg_enemy_bullet.change_x = math.sin(math.radians(player_sprite.angle)) * BULLET_SPEED
-        # Add the bullet to the appropriate list
-        self.reg_enemy_bullet_list.append(reg_enemy_bullet)'''
+            bullet_sprite = arcade.Sprite("/Users/pinaknayak/Documents/Github/Python-Projects/"
+                                          "Python_Arcade/Labs/Iridion/Game_Assets/Active_use/Lasers/Laser_01.png",
+                                          SPRITE_SCALING_PLAYER)
+            bullet_sprite.center_x = self.center_x
+            bullet_sprite.center_y = self.center_y
+            bullet_sprite.change_y = -math.cos(math.radians(player_sprite.angle)) * BULLET_SPEED
+            bullet_sprite.change_x = math.sin(math.radians(player_sprite.angle)) * BULLET_SPEED
+            bullet_sprite.angle = player_sprite.angle
+            self.bullet_list.append(bullet_sprite) #### Figure out why bullets are not being drawn. ####
+            
+        
 
 
+class Player(arcade.Sprite):
+
+    def update(self):
+        """ Move the player """
+        # Move player.
+        # Remove these lines if physics engine is moving player.
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check for out-of-bounds
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
 
 class MyGame(arcade.Window):
-    """
-    Main application class.
-    """
+    """ Main application class. """
 
-    def __init__(self, width, height, title):
-        """
-        Initializer
-        """
-
+    def __init__(self):
+        """ Initializer """
         # Call the parent class initializer
-        super().__init__(width, height, title)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites and Bullets Demo")
 
         # Variables that will hold sprite lists
         self.player_list = None
-        self.bullet_list = None
         self.enemy_list = None
+        self.coin_list = None
+        self.bullet_list = None
 
         # Set up the player info
         self.player_sprite = None
+        self.score = 0
+
+        # Don't show the mouse cursor
+        self.set_mouse_visible(False)
 
         # Track the current state of what key is pressed
         self.left_pressed = False
@@ -149,45 +163,121 @@ class MyGame(arcade.Window):
         self.up_pressed = False
         self.down_pressed = False
 
-        # Set the background color
+
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
+
         """ Set up the game and initialize the variables. """
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
-        self.bullet_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         # Set up the player
-        self.player_sprite = Player("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/Iridion/"
-                                    "Game_Assets/Active_use/Player/spaceShips_001.png",
-                                    SPRITE_SCALING, angle = 180)
+        self.score = 0
+
+        # Image from kenney.nl
+        self.player_sprite = arcade.Sprite("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/"
+                                           "Iridion/Game_Assets/Active_use/Player/spaceShips_001.png",
+                                           SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.center_y = 70
+        self.player_sprite.angle = 180
         self.player_list.append(self.player_sprite)
 
-        # Adding enemies to the screen
-
-        for i in range(ENEMY_COUNT):
-            enemy = Reg_Enemy("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/Iridion/"
-                              "Game_Assets/Active_use/Enemies/Turrent_01.png", SPRITE_SCALING_ENEMY) # @@ angle = 180) - Saving this to see why angle doesn't work when added to the code.
+        for enemy in range(ENEMY_COUNT):
+            enemy = Reg_Enemy("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/"
+                                  "Iridion/Game_Assets/Active_use/Enemies/Turrent_01.png",SPRITE_SCALING_PLAYER)
             enemy.center_x = random.randrange(SCREEN_WIDTH)
-            enemy.center_y = random.randrange(SCREEN_HEIGHT + 20, SCREEN_HEIGHT + 50)
+            enemy.center_y = random.randrange(SCREEN_HEIGHT + 10, SCREEN_HEIGHT + 100)
+            self.enemy_list.append(enemy)
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.coin_list.draw()
+        self.player_list.draw()
+        self.enemy_list.draw()
+        self.bullet_list.draw()
+
+        # Render the text
+        arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.WHITE, 14)
+
+    def reg_enemy_respawn(self):
+
+        for enemy in range(REG_ENEMY_RESPAWN):
+            enemy = Reg_Enemy("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/"
+                                  "Iridion/Game_Assets/Active_use/Enemies/Turrent_01.png",SPRITE_SCALING_PLAYER)
+            enemy.center_x = random.randrange(SCREEN_WIDTH)
+            enemy.center_y = random.randrange(SCREEN_HEIGHT + 30, SCREEN_HEIGHT + 200)
             self.enemy_list.append(enemy)
 
 
-    def on_draw(self):
-        """ Render the screen. """
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
 
-        # Clear the screen
-        self.clear()
+        # Create command to move ship
+        if key == arcade.key.UP:
+            self.up_pressed = True
+            self.update_player_speed()
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+            self.update_player_speed()
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+            self.update_player_speed()
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
+            self.update_player_speed()
 
-        # Draw all the sprites.
-        self.player_list.draw()
-        self.bullet_list.draw()
-        self.enemy_list.draw()
+        # Create command for angle change using z and x
+        if key == arcade.key.X:
+            self.player_sprite.change_angle = -TURN_SPEED
+        elif key == arcade.key.Z:
+            self.player_sprite.change_angle = TURN_SPEED
+
+        # Create command for shooting
+        if key == arcade.key.SPACE:
+            bullet_sprite = arcade.Sprite("/Users/pinaknayak/Documents/Github/Python-Projects/"
+                                          "Python_Arcade/Labs/Iridion/Game_Assets/Active_use/Lasers/Laser_01.png",
+                                          SPRITE_SCALING_PLAYER)
+            bullet_sprite.center_x = self.player_sprite.center_x
+            bullet_sprite.center_y = self.player_sprite.center_y
+            bullet_sprite.change_y = -math.cos(math.radians(self.player_sprite.angle)) * BULLET_SPEED
+            bullet_sprite.change_x = math.sin(math.radians(self.player_sprite.angle)) * BULLET_SPEED
+            bullet_sprite.angle = self.player_sprite.angle
+            self.bullet_list.append(bullet_sprite)
+
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        if key == arcade.key.UP:
+            self.up_pressed = False
+            self.update_player_speed()
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+            self.update_player_speed()
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+            self.update_player_speed()
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
+            self.update_player_speed()
+
+        if key == arcade.key.X or key == arcade.key.Z:
+            self.player_sprite.change_angle = 0
 
     def update_player_speed(self):
 
@@ -204,120 +294,40 @@ class MyGame(arcade.Window):
         elif self.right_pressed and not self.left_pressed:
             self.player_sprite.change_x = MOVEMENT_SPEED
 
-    def on_update(self, delta_time):
+
+    def update(self, delta_time):
         """ Movement and game logic """
 
-        # Call update to move the sprites
+        # Call update on all sprites
+        self.coin_list.update()
         self.player_list.update()
         self.bullet_list.update()
         self.enemy_list.update()
 
-        for enemy in self.enemy_list:
-            enemy.follow_sprite(self.player_sprite)
-            #enemy.reg_aim(self.player_sprite)
-
-        # Checking for collisions with enemies
         for bullet in self.bullet_list:
             hit_list = arcade.check_for_collision_with_list(bullet, self.enemy_list)
             if len(hit_list) > 0:
+                print(hit_list[0])
                 bullet.remove_from_sprite_lists()
             for enemy in hit_list:
                 enemy.remove_from_sprite_lists()
-
-            # Removing bullets if they leave the screen
             if bullet.bottom > SCREEN_HEIGHT:
                 bullet.remove_from_sprite_lists()
-            elif bullet.center_y < 0:
-                bullet.remove_from_sprite_lists()
 
-            elif bullet.center_x < 0:
-                bullet.remove_from_sprite_lists()
-            elif bullet.center_x > SCREEN_WIDTH:
-                bullet.remove_from_sprite_lists()
+        # Calling follow player for regular enemy
 
-        # Calling respawn method
-        #self.respawn()
+        for enemy in self.enemy_list:
+            enemy.follow_player(self.player_sprite)
+            enemy.shoot(self.player_sprite)
 
-        # Change Angle when Z and X are pressed. See on_key_press and on_key_release for more details.
-        self.player_sprite.angle += self.player_sprite.change_angle
+        # Respawn more regular enemies if number falls below 6
 
-    '''def respawn(self):
-        # This respawns regular enemies
-        # Respawn
-        if (len(self.enemy_list) < 6) and random.randrange(0,200) == 1:
-            print("hello")
-            for i in range(ENEMY_COUNT):
-                enemy = Reg_Enemy("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/Iridion/"
-                                  "Game_Assets/Active_use/Enemies/Turrent_01.png", SPRITE_SCALING_ENEMY, angle=180)
-                enemy.center_x = random.randrange(SCREEN_WIDTH)
-                enemy.center_y = random.randrange(SCREEN_HEIGHT + 20, SCREEN_HEIGHT + 50)
-                self.enemy_list.append(enemy)'''
-
-
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
-
-        # Controls player movement
-        if key == arcade.key.UP:
-            self.up_pressed = True
-            self.update_player_speed()
-        elif key == arcade.key.DOWN:
-            self.down_pressed = True
-            self.update_player_speed()
-        elif key == arcade.key.LEFT:
-            self.left_pressed = True
-            self.update_player_speed()
-        elif key == arcade.key.RIGHT:
-            self.right_pressed = True
-            self.update_player_speed()
-
-        # Creating a bullet when the user presses the spacebar
-        if key == arcade.key.SPACE:
-            bullet = arcade.Sprite("/Users/pinaknayak/Documents/Github/Python-Projects/Python_Arcade/Labs/Iridion/"
-                                   "Game_Assets/Active_use/Lasers/Laser_01.png", SPRITE_SCALING_LASER)
-
-            bullet.change_y = -math.cos(math.radians(self.player_sprite.angle)) * BULLET_SPEED
-            bullet.change_x = math.sin(math.radians(self.player_sprite.angle)) * BULLET_SPEED
-            bullet.center_x = self.player_sprite.center_x
-            bullet.center_y = self.player_sprite.center_y
-            bullet.angle = self.player_sprite.angle
-
-            # Add the bullet to the appropriate list
-            self.bullet_list.append(bullet)
-
-        # Change angle if Z or X are pressed.
-        if key == arcade.key.Z:
-            self.player_sprite.change_angle = 5
-        elif key == arcade.key.X:
-            self.player_sprite.change_angle = -5
-
-
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
-        if key == arcade.key.UP:
-            self.up_pressed = False
-            self.update_player_speed()
-        elif key == arcade.key.DOWN:
-            self.down_pressed = False
-            self.update_player_speed()
-        elif key == arcade.key.LEFT:
-            self.left_pressed = False
-            self.update_player_speed()
-        elif key == arcade.key.RIGHT:
-            self.right_pressed = False
-            self.update_player_speed()
-
-        # Stop changing angle if Z or X are released.
-        if key == arcade.key.Z:
-            self.player_sprite.change_angle = 0
-        if key == arcade.key.X:
-            self.player_sprite.change_angle = 0
+        if (len(self.enemy_list) < 6):
+            self.reg_enemy_respawn()
 
 
 def main():
-    """ Main function """
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = MyGame()
     window.setup()
     arcade.run()
 
